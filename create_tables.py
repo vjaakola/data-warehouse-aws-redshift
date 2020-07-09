a2323371ac992f86c3a556dd68d85a9f6ae63dbb
Vesa Jaakola <vesa.jaakola@hel.fi>
2020-07-09 21:12:00 +0300

    Create create_tables.py

---
+++ create_tables.py
@@ -0,0 +1,43 @@
+import configparser
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        try:
+            cur.execute(query)
+            conn.commit()
+        except psycopg2.Error as e:
+            print("Error: Dropping table: " +query)
+            print(e)
+
+    print("Tables dropped")
+
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        try:
+            cur.execute(query)
+            conn.commit()
+        except psycopg2.Error as e:
+            print("Error: Creating table: " + query)
+            print(e)
+    print("Tables created")
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
