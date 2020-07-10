a661e1d3f266098b3caf488fe66d1fe54fdd2881
Vesa Jaakola <vesa.jaakola@hel.fi>
2020-07-10 10:26:58 +0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,7 +2,11 @@ import configparser
 import psycopg2
 from sql_queries import copy_table_queries, insert_table_queries
 
-
+""" drop tables from drop table queries and printing the status
+INPUTS:
+* cur: cursor variable of the database
+* conn: connection variable of the database
+"""
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
         print('Executing loading staging tables' +query)
@@ -10,13 +14,21 @@ def load_staging_tables(cur, conn):
         conn.commit()
         print('Executed loading staging tables' +query)
  
-    
+""" create tables from drop table queries and printing the status
+INPUTS:
+* cur: cursor variable of the database
+* conn: connection variable of the database
+"""    
 def insert_tables(cur, conn):
     for query in insert_table_queries:
+        print('Executing create'+query)
         cur.execute(query)
         conn.commit()
 
+        
+""" main function to connect database, using credentials from configuration file (dwh.cfg), dropping and creating tables
 
+""" 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
